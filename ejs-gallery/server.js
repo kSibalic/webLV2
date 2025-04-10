2af8f42f3704d471f820bd57db52f071148f7ab3
kSibalic <sibalickarlo@gmail.com>
2025-04-11 00:39:15 +0200

    Initial commit

---
+++ ejs-gallery/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/images', (req, res) => {
+    const dataPath = path.join(__dirname, 'images.json');
+    const images = JSON.parse(fs.readFileSync(dataPath));
+    res.render('slike', { images });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server je pokrenut na portu ${PORT}`);
+});
